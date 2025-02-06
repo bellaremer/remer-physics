bc4be6a2925843660e084e6de1a93a949f78a14d
shoshana remer <bellaremer@bellas-air.mynetworksettings.com>
2025-02-06 10:35:23 -0500

    updated Force added to Forces main

--- src/main/java/remer/physics/Forces.java
+++ src/main/java/remer/physics/Forces.java
@@ -12,35 +12,6 @@ public class Forces
         System.out.println("Resultant Force is: ");
         System.out.println("Degree: " + resultForce.getDegree());
         System.out.println("Magnitude: " + resultForce.getMagnitude());
-
-
-       /* double force1 = 10;
-        double angle1degrees = 32;
-
-        double force2 = 10;
-        double angle2degrees = 105;
-
-        double angle1 = Math.toRadians(angle1degrees);
-        double angle2 = Math.toRadians(angle2degrees);
-
-        double force1X = force1 * Math.cos(angle1);
-        double force1Y = force1 * Math.sin(angle1);
-
-        double force2X = force2 * Math.cos(angle2);
-        double force2Y = force2 * Math.sin(angle2);
-
-        double forceX = force1X + force2X;
-        double forceY = force1Y + force2Y;
-
-        double totalMagnitude = Math.sqrt(forceX * forceX + forceY * forceY);
-
-        double resultAngleRadian = Math.atan2(forceY, forceX);
-        double resultAngleDegrees = Math.toDegrees(resultAngleRadian);
-
-        System.out.println("The resulting force magnitude is: " + totalMagnitude);
-        System.out.println("The resulting force angle is: " + resultAngleDegrees);
-        */
-
     }
 
 }
